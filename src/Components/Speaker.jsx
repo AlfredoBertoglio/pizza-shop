fc7d88abb6793f07553b5fd9b4a807639fde10d4
Alfredo Bertoglio <alfredobertoglio@gmail.com>
2025-04-19 18:52:12 +0800

    speaker autoplay true removed

--- src/Components/Speaker.jsx
+++ src/Components/Speaker.jsx
@@ -3,13 +3,13 @@ import { Howl } from 'howler';
 import PizzaVoiceMale from '../../public/Sounds/PizzaVoiceMale.mp3';
 
 export default function Speaker()
-{       
-        const grito = new Howl({
-            src: [PizzaVoiceMale],
-            // autoplay: true,
-            loop: false,
-            volume: 0.3
-        });   
+{   // autoplay: true removed  
+
+    const grito = new Howl({
+        src: [PizzaVoiceMale],
+        loop: false,
+        volume: 0.3
+    });   
     
     return(
        grito.play()
