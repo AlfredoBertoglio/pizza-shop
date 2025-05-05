59222b76ff58c4eda4a85cd75ca7ce57b6ea52e2
Alfredo Bertoglio <alfredobertoglio@gmail.com>
2025-05-05 19:07:37 +0800

    lines space remove from experience

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -6,20 +6,17 @@ import CameraGsap from './Components/CameraGsap';
 
 
 export default function Experience ()
-{   
-
+{ 
         return (
           <>
 
     <SoftShadows frustum={ 3.75 } size={ 25 } near={ 9.5 } samples={ 20 } rings={ 11 } />
-
 
     <Environment
         background
         files={'./environmentMaps/solitude_night_4k.hdr'}
         blur={0.45}
     />
-
     <CameraGsap />
 
     <directionalLight 
